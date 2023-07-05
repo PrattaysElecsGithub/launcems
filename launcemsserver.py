59072527bd85565c257be7c6f0622e71e51d1e20
OpenPrattay <orginalprattaysarkar@gmail.com>
2023-07-05 18:54:02 +0530

    Initial commit

---
+++ launcemsserver.py
@@ -0,0 +1,30 @@
+# LaunceMS Server Project
+# Made Proudly in India, By Prattay Sarkar, aka. Prattay's Elecs GITHUB
+
+import configparser
+from colorama import Fore
+from flask import Flask, render_template
+
+print(Fore.GREEN + "|----+ LaunceMS Server +----|")
+print(Fore.GREEN + "|+- Made Proudly In India -+|")
+print(Fore.GREEN + "|+- By Prattay Sarvar, WB -+|")
+print(Fore.GREEN + "|___________________________|\n")  # TODO: Fix Github Respoitory upload problem
+print(Fore.BLUE + "|+- Trying To Start LaunceMS Server...")
+
+app = Flask(__name__, template_folder="htmlfiles")
+
+config = configparser.ConfigParser()
+config.read('conf.conf')
+port = int(config.get('system', 'port'))
+indexfile = str(config.get('sysdisk', 'indexfile'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    print(Fore.BLUE + "|+- Switching To Flask Output For Error Checking...\n")
+
+    app.run(host='0.0.0.0', port=port)
