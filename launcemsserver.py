b8cdcd4842c702a5906a0e84210390813708b0ee
Prattay Sarkar <orginalprattaysarkar@gmail.com>
2023-07-13 16:15:41 +0530

    Design and Code Readability Updated

--- launcemsserver.py
+++ launcemsserver.py
@@ -1,15 +1,15 @@
 # LaunceMS Server Project
-# Made Proudly in India, By Prattay Sarkar, aka. Prattay's Elecs GITHUB
+# Made Proudly in India, By Prattay Sarkar, aka. Prattay'sElecsGithub
 
 import configparser
 from colorama import Fore
 from flask import Flask, render_template
 
-print(Fore.GREEN + "|----+ LaunceMS Server +----|")
-print(Fore.GREEN + "|+- Made Proudly In India -+|")
-print(Fore.GREEN + "|+- By Prattay Sarvar, WB -+|")
-print(Fore.GREEN + "|___________________________|\n")  # TODO: Fix Github Respoitory upload problem
-print(Fore.BLUE + "|+- Trying To Start LaunceMS Server...")
+print(Fore.GREEN + "╔════╣ LaunceMS Server ╠════╗")
+print(Fore.GREEN + "║+- Made Proudly In India -+║")
+print(Fore.GREEN + "║+- By Prattay Sarvar, WB -+║")
+print(Fore.GREEN + "╚═══════════════════════════╝\n")
+print(Fore.BLUE + "|+- Trying To Start LaunceMS Dev Server...")
 
 app = Flask(__name__, template_folder="htmlfiles")
 
@@ -19,10 +19,13 @@ port = int(config.get('system', 'port'))
 indexfile = str(config.get('sysdisk', 'indexfile'))
 
 
+# ═══════════════  USER AREA  ══════════════════
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+# ══════════════════════════════════════════════
 
 if __name__ == '__main__':
     print(Fore.BLUE + "|+- Switching To Flask Output For Error Checking...\n")
